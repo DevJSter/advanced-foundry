76aad75e2163d65c596eb89413fe5721855d59bf
Shubham Tiwari <shubht3303@gmail.com>
2025-01-04 15:20:59 +0530

    deploy our tokens script

--- ERC20-Token/script/DeployOurToken.s.sol
+++ ERC20-Token/script/DeployOurToken.s.sol
@@ -5,11 +5,14 @@ import {Script} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 
 contract DeployOurToken is Script{
-        uint256 public constant INITIIAL_SUPPLY = 1000 ether; 
-    function run() external {
+    
+    uint256 public constant INITIIAL_SUPPLY = 1000 ether; 
+    
+    function run() external returns(OurToken) {
         vm.startBroadcast();
-         new OurToken(INITIIAL_SUPPLY);
+         OurToken ot = new OurToken(INITIIAL_SUPPLY);
         vm.stopBroadcast();
+        return ot;
     } 
 
 }
---
+++ ERC20-Token/test/OurTokenTest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol"
+
+;
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+
+    function setUp()  public {
+        deployer = new DeployOurToken();
+        ourToken = OurToken().run;
+        
+    }
+}
