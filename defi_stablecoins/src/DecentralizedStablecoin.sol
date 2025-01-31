2023c8667dcf0923dacbbea243b36450f848ccda
Shubham Tiwari <shubht3303@gmail.com>
2025-01-31 18:45:56 +0530

    stablecoins

--- defi_stablecoins/src/DecentralizedStablecoin.sol
+++ defi_stablecoins/src/DecentralizedStablecoin.sol
@@ -1,4 +1,5 @@
 // SPDX Licesne-Identifier: MIT
 pragma solidity  ^0.8.0;
 
-import "./IERC20.sol";
+import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
